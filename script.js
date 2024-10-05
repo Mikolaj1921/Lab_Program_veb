9d721e4ddd7784ee261d7a4c57f10d8490831a01
Mikolaj231 <mikolajmelnyk16@gmail.com>
2024-10-05 12:30:52 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-(function () {
-  //TODO
-})();
+document.getElementById("ex1_button").addEventListener("click", function() {
+  let numbers = [];
+  for (let i = 0; i <= 9; i++) {
+      numbers.push(i);
+  }
+  document.getElementById("ex1_content").textContent = numbers.join(", ");
+});
